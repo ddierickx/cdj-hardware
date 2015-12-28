eff88796d32781c9863ccf4016e29c7c0ae7b2b3
Jordy Vlassembrouck <jordy.vlassembrouck@aca-it.be>
2015-12-28 16:25:59 +0000

    get ultrasonic sensor working with RPi.

--- motor-test/test.py
+++ motor-test/test.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 
 import RPi.GPIO as GPIO
+import time as TIME
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(13, GPIO.OUT)
 
-print("Test")
+#starting from the left, 'left' resembles 0 degrees,
+left = (0.6/20.0) * 100
+#middle resembles a 90 degree angle
+middle = (1.5/20.0) * 100
+#right resembles 180 degrees
+right = (2.5/20.0) * 100
+
+p = GPIO.PWM(13, 50)
+p.start(left)
+TIME.sleep(1)
+p.start(right)
+TIME.sleep(1)
+p.start(middle)
+TIME.sleep(1)
+p.stop()
+GPIO.cleanup()
